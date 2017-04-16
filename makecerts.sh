b3459eb8f3660f2194406c5fb9c68a29c5d3ff94
sbrendtro <steven.brendtro@gmail.com>
2017-04-15 20:47:43 -0500

    Fixed invalid paths for symlinks.

--- makecerts.sh
+++ makecerts.sh
@@ -7,14 +7,13 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
     echo
     rm -rf ssl/openach/openach.crt ssl/openach/openach.key
-    ln -s /ssl/openach/openach-self-signed.key ssl/openach/openach.key
-    ln -s /ssl/openach/openach-self-signed.crt ssl/openach/openach.crt
+    cd ssl/openach/ && \
+        ln -s openach-self-signed.key openach.key && \
+        ln -s openach-self-signed.crt openach.crt
+    cd ../..
     echo "Installed new SSL key as:  ssl/openach/openach.key"
     echo "Installed new SSL cert as: ssl/openach/openach.crt"
     echo "NOTE: You will need to restart your containers for the change to take effect."
 else
     echo "Generated the keys, but they are not installed.  You will need to symlink (or copy) them to ssl/openach/openach.key and ssl/openach/openach.crt"
 fi
-
-
-
