41be634738bf071df9d017fe8e2cdf7542439c04
sbrendtro <steven.brendtro@gmail.com>
2020-08-19 11:00:41 -0500

    Fixed syntax error in makecerts.sh

--- makecerts.sh
+++ makecerts.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 openssl req -x509 -nodes -days 365 -newkey rsa:4086 -keyout ssl/openach/openach-self-signed.key -out ssl/openach/openach-self-signed.crt
 echo 
 echo "Ready to install new key and cert as ssl/openach/openach.key and ssl/openach/openach.crt"
